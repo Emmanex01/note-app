71cdefde21a946cf0223e67a6894ed9e5f9ea53a
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-09-11 19:39:25 +0100

    Added ideaInput component

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header'
+import IdeaInput from './components/IdeaInput'
 import Notes from './components/Notes'
 import SideBar from './components/SideBar'
 
@@ -7,16 +8,19 @@ function App() {
   return (
     <div className='flex h-screen'>
       {/* Left */}
-      <div className='w-1/4 border-r border-gray-300 p-4'>
+      <div className='w-1/5 border-r border-gray-300 p-4'>
         <h1 className='text-4xl font-bold ml-6'>LOGO</h1>
         <SideBar/>
       </div>
       {/* Right */}
-      <div className=' w-3/4 p-4'>
+      <div className=' w-4/5 p-4 overflow-y-auto h-full'>
         <Header/>
-        <div className='flex flex-col md:flex-row mt-4'>
+        <div className='flex flex-col gap-12 md:flex-row mt-4'>
           <Notes/>
-          <div className='md:w-2/3'>Right</div>
+          <div className='md:w-2/3'>
+            {/* Idea Input Component */}
+            <IdeaInput/>
+          </div>
         </div>
       </div>
     </div>
---
+++ note-app/src/components/IdeaInput.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import noteIcon from '../assets/noteIcon.png';
+import { FaLightbulb } from "react-icons/fa6";
+
+const IdeaInput = () => {
+  return (
+    <div>
+      <div className='flex justify-center'>
+        <img src={noteIcon} alt="note Icon" className=''/>
+      </div>
+      <div className='flex flex-col gap-6'>
+        <div className='flex items-center'>
+            <h1 className='font-medium text-3xl'>Write down your Ideas</h1>
+            <FaLightbulb className='w-10 h-10 text-amber-300'/>
+        </div>
+        
+        <div className='flex gap-4 font-medium text-yellow-700'>
+            <span>#ideas</span>
+            <span>#to-do's</span>
+            <span>#morning's</span>
+        </div>
+        <div>
+            <textarea 
+                name="" id="" 
+                className=' w-full min-h-[150px] p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-300 resize-none'
+                placeholder="What’s on your mind today? Write freely about your goals, wild ideas, or even just random thoughts you don’t want to lose. Think of this space as your personal scratchpad—messy is okay, big dreams are welcome."
+            ></textarea>
+        </div>
+        <div className='flex gap-4 justify-end'>
+            <button className='font-medium text-xl bg-gray-200 p-4 rounded-4xl w-12 h-12 flex justify-center items-center'>+</button>
+            <button className='font-medium bg-gray-200 p-4 rounded-4xl w-12 h-12 flex justify-center items-center'>Aa</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IdeaInput
